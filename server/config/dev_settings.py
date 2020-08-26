21466e058d09a555585ec4d03cfbffc619eb16ad
Diego Cardoso Nunes <diegocar.nunes@gmail.com>
2020-08-25 22:09:13 -0300

    Default django project template

---
+++ server/config/dev_settings.py
@@ -0,0 +1,24 @@
+from .settings import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = [
+    '192.168.99.100',
+    'localhost',
+    '127.0.0.1',
+    '*'
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'PORT': os.environ.get('DB_PORT'),
+        'HOST': os.environ.get('DB_HOST'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD')
+    }
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = BASE_DIR / 'emails'
---
+++ server/config/production_settings.py
@@ -0,0 +1,18 @@
+from .settings import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'PORT': os.environ.get('DB_PORT'),
+        'HOST': os.environ.get('DB_HOST'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD')
+    }
+}
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
